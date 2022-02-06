33a52a1deb0b6aaba5c8e07ada3b5b3fd64a8fa0
billynguyenlss <billynguyen.lss@gmail.com>
2022-02-06 08:25:49 +0700

    add black and isort

--- setup.py
+++ setup.py
@@ -1,34 +1,30 @@
-from setuptools import setup, find_packages
 import pathlib
 
+from setuptools import find_packages, setup
+
 here = pathlib.Path(__file__).parent.resolve()
-long_description = (here / 'README.md').read_text(encoding='utf-8')
+long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name='projectmlincvmediapipe',
-    version='0.1.0',
-    description='demo packaging python project using Mediapipe Meet Segmentation pretrained model',
+    name="projectmlincvmediapipe",
+    version="0.1.0",
+    description="demo packaging python project using Mediapipe Meet Segmentation pretrained model",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/billynguyenlss/projectMLinComputerVision',
-    author='billynguyen',
-    author_email='billynguyen.lss@gmail.com',
-    package_dir={'': 'src'},
-    packages=find_packages(where='src'),  # Required
-    python_requires='>=3.6, <4',
+    long_description_content_type="text/markdown",
+    url="https://github.com/billynguyenlss/projectMLinComputerVision",
+    author="billynguyen",
+    author_email="billynguyen.lss@gmail.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),  # Required
+    python_requires=">=3.6, <4",
     install_requires=[
-        'numpy>=1.22',
-        'opencv-contrib-python>=4.5',
-        'tensorflow>=2.6',
-        'tflite_runtime'
+        "numpy>=1.22",
+        "opencv-contrib-python>=4.5",
+        "tensorflow>=2.6",
+        "tflite_runtime",
     ],
     package_data={
-        'projectmlincvmediapipe': ['model_float16_quant.tflite','portrait.jpg']
+        "projectmlincvmediapipe": ["model_float16_quant.tflite", "portrait.jpg"]
     },
-
-    entry_points={
-        'console_scripts': [
-            'demo=projectmlincvmediapipe.demo:main'
-        ]
-    }
+    entry_points={"console_scripts": ["demo=projectmlincvmediapipe.demo:main"]},
 )
