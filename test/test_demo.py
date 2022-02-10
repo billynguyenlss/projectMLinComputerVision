0895ee6cba9f7baf7bfc080f143691df327ac0f8
billynguyenlss <billynguyen.lss@gmail.com>
2022-02-10 16:30:25 +0700

    add test_small_image and test_large_image, change python versions for build

--- test/test_demo.py
+++ test/test_demo.py
@@ -23,8 +23,36 @@ def test_regression():
 
 
 def test_small_image():
-    pass
+    for img_name in ["portrait_small_1.jpg", "portrait_small_2.jpg"]:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        img_path = os.path.join(dir_path, img_name)
+        img = cv2.imread(img_path)
+
+        h = img.shape[0]
+        w = img.shape[1]
+
+        output, out1, out2 = demo.main("portrait.jpg")
+        print("output:", output.shape)
+        print("out1:", out1.shape)
+        print("out2:", out2.shape)
+        assert output.shape == (1, 144, 256, 2)
+        assert out1.shape == (h, w)
+        assert out2.shape == (h, w)
 
 
 def test_large_image():
-    pass
+    for img_name in ["portrait_large_1.jpg", "portrait_large_2.jpg"]:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        img_path = os.path.join(dir_path, img_name)
+        img = cv2.imread(img_path)
+
+        h = img.shape[0]
+        w = img.shape[1]
+
+        output, out1, out2 = demo.main("portrait.jpg")
+        print("output:", output.shape)
+        print("out1:", out1.shape)
+        print("out2:", out2.shape)
+        assert output.shape == (1, 144, 256, 2)
+        assert out1.shape == (h, w)
+        assert out2.shape == (h, w)
