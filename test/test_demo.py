3eaa2a6ba3bd87291c357e24818ce14b14cd42ba
billynguyenlss <billynguyen.lss@gmail.com>
2022-02-10 15:48:57 +0700

    week2b - modify test.py, add output.shape

--- test/test_demo.py
+++ test/test_demo.py
@@ -4,7 +4,7 @@ from src.projectmlincvmediapipe import demo
 def test_regression():
     output, out1, out2 = demo.main("portrait.jpg")
     print("output:", output)
-    assert output == (1, 144, 256, 2)
+    assert output.shape == (1, 144, 256, 2)
     assert out1 == (144, 256)
     assert out2 == (144, 256)
 
