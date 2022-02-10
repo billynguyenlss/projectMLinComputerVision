4daa5aeb62301b1f9f95bbbc0572de95de7e7262
billynguyenlss <billynguyen.lss@gmail.com>
2022-02-10 15:57:13 +0700

    week2b - modify test_regression() in test.py - add shape to assert

--- test/test_demo.py
+++ test/test_demo.py
@@ -3,10 +3,12 @@ from src.projectmlincvmediapipe import demo
 
 def test_regression():
     output, out1, out2 = demo.main("portrait.jpg")
-    print("output:", output)
+    print("output:", output.shape)
+    print("out1:", out1.shape)
+    print("out2:", out2.shape)
     assert output.shape == (1, 144, 256, 2)
-    assert out1 == (144, 256)
-    assert out2 == (144, 256)
+    assert out1.shape == (144, 256)
+    assert out2.shape == (144, 256)
 
 
 def test_small_image():
