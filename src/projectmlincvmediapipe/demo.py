430c6db41f513330391dfbd1ed1aa099b3dd94ee
billynguyenlss <billynguyen.lss@gmail.com>
2022-02-23 18:58:47 +0700

    add CI workflow

--- src/projectmlincvmediapipe/demo.py
+++ src/projectmlincvmediapipe/demo.py
@@ -3,6 +3,7 @@ import sys
 
 import cv2
 import numpy as np
+from cv2 import CV_16SC1
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
@@ -11,10 +12,15 @@ try:
 except:
     from tensorflow.lite.python.interpreter import Interpreter
 
+# dir_path = os.path.dirname(os.path.realpath(__file__))
+model_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "model/model_float16_quant.tflite")
 
-def main(img_name="portrait.jpg"):
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+def main(img_name="img/portrait.jpg"):
+    # dir_path = os.path.dirname(os.path.realpath(__file__))
+    dir_path = os.path.abspath(os.path.dirname(__file__))
     img_path = os.path.join(dir_path, img_name)
+    # # img_path = img_name
     img = cv2.imread(img_path)
     # img = cv2.imread('portrait.jpg')
 
@@ -28,10 +34,8 @@ def main(img_name="portrait.jpg"):
     img = img[np.newaxis, :, :, :]
 
     # Tensorflow Lite
-    model_path = os.path.join(dir_path, "model_float16_quant.tflite")
     interpreter = Interpreter(model_path=model_path, num_threads=4)
 
-    # interpreter = Interpreter(model_path='model_float16_quant.tflite', num_threads=4)
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()[0]["index"]
     output_details = interpreter.get_output_details()[0]["index"]
@@ -40,23 +44,31 @@ def main(img_name="portrait.jpg"):
     interpreter.invoke()
     output = interpreter.get_tensor(output_details)
 
-    print(output.shape)
+    # print(output.shape)
     out1 = output[0][:, :, 0]
     out2 = output[0][:, :, 1]
 
     out1 = np.invert((out1 > 0.5) * 255)
     out2 = np.invert((out2 > 0.5) * 255)
 
-    print("out1:", out1.shape)
-    print("out2:", out2.shape)
+    # print("out1:", out1.shape)
+    # print("out2:", out2.shape)
 
     out1 = cv2.resize(np.uint8(out1), (w, h))
     out2 = cv2.resize(np.uint8(out2), (w, h))
 
-    cv2.imwrite("out1.jpg", out1)
+    cv2.imwrite("out1.jpg", out1, CV_16SC1)
     cv2.imwrite("out2.jpg", out2)
 
     out3 = cv2.ximgproc.jointBilateralFilter(out2, out1, 8, 75, 75)
 
     cv2.imwrite("out3.jpg", out3)
-    return (output, out1, out2)
+    print("output:", output.shape)
+    print("out1:", out1.shape)
+    print("out2:", out2.shape)
+    print("out3:", out3.shape)
+    return (output, out1, out2, out3)
+
+
+if __name__ == "__main__":
+    output, out1, out2, out3 = main()
--- test/test_demo.py
+++ test/test_demo.py
@@ -1,31 +1,55 @@
 import os
+import os.path as osp
+import sys
 
 import cv2
+import numpy as np
+import pytest
+from cv2 import IMREAD_GRAYSCALE
 
-from src.projectmlincvmediapipe import demo
+from projectmlincvmediapipe import demo
+
+sys.path.append(osp.dirname(osp.realpath(__file__)))
 
 
-def test_regression():
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    img_path = os.path.join(dir_path, "portrait.jpg")
+@pytest.mark.parametrize("img_name", ["img/portrait.jpg"])
+def test_regression(img_name):
+    def mse(imageA, imageB):
+        err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+        err /= float(imageA.shape[0] * imageA.shape[1])
+        return err
+
+    dir_path = os.path.abspath(os.path.dirname(__file__))
+    img_path = os.path.join(dir_path, img_name)
+
+    out1_true = cv2.imread(os.path.join(dir_path, "img/out1_true.jpg"), IMREAD_GRAYSCALE)
+    out2_true = cv2.imread(os.path.join(dir_path, "img/out2_true.jpg"), IMREAD_GRAYSCALE)
+    out3_true = cv2.imread(os.path.join(dir_path, "img/out3_true.jpg"), IMREAD_GRAYSCALE)
+    print("true out1:", out1_true.shape)
+    print("true out2:", out2_true.shape)
+    print("true out3:", out3_true.shape)
     img = cv2.imread(img_path)
+    print("img:", img.shape)
 
     h = img.shape[0]
     w = img.shape[1]
 
-    output, out1, out2 = demo.main("portrait.jpg")
+    output, out1, out2, out3 = demo.main(img_path)
     print("output:", output.shape)
     print("out1:", out1.shape)
     print("out2:", out2.shape)
     assert output.shape == (1, 144, 256, 2)
     assert out1.shape == (h, w)
     assert out2.shape == (h, w)
+    assert mse(out1, out1_true) < 0.05
+    assert mse(out2, out2_true) < 0.05
+    assert mse(out3, out3_true) < 0.05
 
 
-def test_small_image():
-    # for img_name in ["portrait_small_1.jpg", "portrait_small_2.jpg"]:
-    img_name = "portrait_small_1.jpg"
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+@pytest.mark.parametrize("img_name", ["img/portrait_small_1.jpg", "img/portrait_small_2.jpg"])
+def test_small_image(img_name):
+
+    dir_path = os.path.abspath(os.path.dirname(__file__))
     img_path = os.path.join(dir_path, img_name)
     print(os.getcwd())
     img = cv2.imread(img_path)
@@ -33,7 +57,7 @@ def test_small_image():
     h = img.shape[0]
     w = img.shape[1]
 
-    output, out1, out2 = demo.main(img_name)
+    output, out1, out2, out3 = demo.main(img_path)
     print("output:", output.shape)
     print("out1:", out1.shape)
     print("out2:", out2.shape)
@@ -42,10 +66,10 @@ def test_small_image():
     assert out2.shape == (h, w)
 
 
-def test_large_image():
-    # for img_name in ["portrait_large_1.jpg", "portrait_large_2.jpg"]:
-    img_name = "portrait_large_1.jpg"
-    dir_path = os.path.dirname(os.path.realpath(__file__))
+@pytest.mark.parametrize("img_name", ["img/portrait_large_1.jpg", "img/portrait_large_2.jpg"])
+def test_large_image(img_name):
+
+    dir_path = os.path.abspath(os.path.dirname(__file__))
     img_path = os.path.join(dir_path, img_name)
     print(os.getcwd())
     img = cv2.imread(img_path)
@@ -53,10 +77,16 @@ def test_large_image():
     h = img.shape[0]
     w = img.shape[1]
 
-    output, out1, out2 = demo.main(img_name)
+    output, out1, out2, out3 = demo.main(img_path)
     print("output:", output.shape)
     print("out1:", out1.shape)
     print("out2:", out2.shape)
     assert output.shape == (1, 144, 256, 2)
     assert out1.shape == (h, w)
     assert out2.shape == (h, w)
+
+
+if __name__ == "__main__":
+    test_regression()
+    test_large_image()
+    test_small_image()
